fe03851c1e9231eb206ce6d233137a10bce515ab
Vineet Bhatt <wildframevineet@gmail.com>
2026-04-04 19:13:26 +0530

    Added delete and balance system

--- frontend/app.js
+++ frontend/app.js
@@ -3,14 +3,42 @@ let transactions = JSON.parse(localStorage.getItem("data")) || [];
 const form = document.getElementById("finance-form");
 const list = document.getElementById("list");
 
+const balanceEl = document.getElementById("balance");
+const incomeEl = document.getElementById("income");
+const expenseEl = document.getElementById("expense");
+
 function render() {
   list.innerHTML = "";
 
-  transactions.forEach((t) => {
+  let income = 0;
+  let expense = 0;
+
+  transactions.forEach((t, index) => {
     const li = document.createElement("li");
-    li.textContent = `${t.type.toUpperCase()}: ${t.desc} - ₹${t.amount}`;
+
+    li.innerHTML = `
+      ${t.type.toUpperCase()}: ${t.desc} - ₹${t.amount}
+      <button onclick="deleteItem(${index})">X</button>
+    `;
+
     list.appendChild(li);
+
+    if (t.type === "income") {
+      income += Number(t.amount);
+    } else {
+      expense += Number(t.amount);
+    }
   });
+
+  balanceEl.textContent = income - expense;
+  incomeEl.textContent = income;
+  expenseEl.textContent = expense;
+}
+
+function deleteItem(index) {
+  transactions.splice(index, 1);
+  localStorage.setItem("data", JSON.stringify(transactions));
+  render();
 }
 
 form.addEventListener("submit", function (e) {
@@ -23,7 +51,6 @@ form.addEventListener("submit", function (e) {
   const newTransaction = { desc, amount, type };
 
   transactions.push(newTransaction);
-
   localStorage.setItem("data", JSON.stringify(transactions));
 
   render();
